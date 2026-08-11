09c104ad80c4db3f599594f0428ace5044bc86e1
AakifCodes <akifalvi2005@gmail.com>
2026-08-11 08:58:02 +0500

    Add basic React and Vite React projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Tea from "./Tea";
+
+function App() {
+  return (
+    <>
+    <Tea/>
+    <h1>Tea and react</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Tea from "./tea"
+
+function App() {
+  return (
+    <>
+    <h1>tea and cup with vite</h1>
+    <Tea/>
+    </>
+  )
+}
+
+export default App
