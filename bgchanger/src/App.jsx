bcbf7e80b5b7f614a2e5d35063997395f678af4c
AakifCodes <akifalvi2005@gmail.com>
2026-08-13 18:58:44 +0500

    tailwind setup and check

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -2,10 +2,17 @@ import './App.css'
 import Button from './components/button'
 
 function App() {
+  const [color, setcolor] = useState("olive")
 
   return (
     <>
-      <Button colorName = "Red" />
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+
+    </div>
+    <div className='fixed flex flec-wrap justify-center botton-12 inset-x-0 px-2'>
+      <Button colorName = {color} />
+
+    </div>
     </>
   )
 }
