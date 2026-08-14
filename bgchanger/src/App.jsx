45ab6190a138a2eaa236ccd8e7cb1a36827e7a78
AakifCodes <akifalvi2005@gmail.com>
2026-08-14 11:03:29 +0500

    Set set color function

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,24 +1,36 @@
 import './App.css'
 import Button from './components/button'
-import {useState} from 'react'
+import { useState } from 'react'
 
 function App() {
-  const [color, setcolor] = useState("olive")
+  const [color, setColor] = useState("olive")
 
   return (
     <>
-    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
-
-    </div>
-    <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-      <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-4xl'>
-        <Button colorName = "Red" bgColor = "red"/>
-        <Button colorName = "Green" bgColor = "green"/>
-        <Button colorName = "Blue" bgColor = "blue"/>
-
+      <div 
+        className='w-full h-screen duration-200' 
+        style={{ backgroundColor: color }}
+      />
+      
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
+          <Button 
+            colorName="Red" 
+            bgColor="red" 
+            onClick={() => setColor("red")} 
+          />
+          <Button 
+            colorName="Green" 
+            bgColor="green" 
+            onClick={() => setColor("green")} 
+          />
+          <Button 
+            colorName="Blue" 
+            bgColor="blue" 
+            onClick={() => setColor("blue")} 
+          />
+        </div>
       </div>
-    </div>
-
     </>
   ) 
 }
--- bgchanger/src/components/button.jsx
+++ bgchanger/src/components/button.jsx
@@ -1,5 +1,11 @@
-export default function Button({colorName = "Color", bgColor = "white"}){
-    return(
-        <button style={{ backgroundColor: bgColor }} className="outline-none px-4 py-1 rounded-full text-white shadow-lg">{colorName}</button>
-    )
+export default function Button({ colorName = "Color", bgColor = "white", onClick }) {
+  return (
+    <button 
+      onClick={onClick}
+      style={{ backgroundColor: bgColor }} 
+      className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+    >
+      {colorName}
+    </button>
+  );
 }
