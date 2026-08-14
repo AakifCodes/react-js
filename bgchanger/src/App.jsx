0f856a6495f970408b6d1093fd6be04865daab25
AakifCodes <akifalvi2005@gmail.com>
2026-08-14 11:21:35 +0500

    Complete background color changer project

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -17,17 +17,62 @@ function App() {
           <Button 
             colorName="Red" 
             bgColor="red" 
+            textColor='white'
             onClick={() => setColor("red")} 
           />
           <Button 
             colorName="Green" 
             bgColor="green" 
+            textColor='white'
             onClick={() => setColor("green")} 
           />
           <Button 
             colorName="Blue" 
             bgColor="blue" 
+            textColor='white'
             onClick={() => setColor("blue")} 
+          />
+          <Button 
+            colorName="Olive" 
+            bgColor="olive" 
+            onClick={() => setColor("olive")} 
+          />
+          <Button 
+            colorName="Gray" 
+            bgColor="gray" 
+            onClick={() => setColor("gray")} 
+          />
+          <Button 
+            colorName="Yellow" 
+            bgColor="yellow" 
+            onClick={() => setColor("yellow")} 
+          />
+          <Button 
+            colorName="Pink" 
+            bgColor="pink" 
+            onClick={() => setColor("pink")} 
+          />
+          <Button 
+            colorName="Purple" 
+            bgColor="purple"
+            textColor='white' 
+            onClick={() => setColor("purple")} 
+          />
+          <Button 
+            colorName="Lavender" 
+            bgColor="lavender" 
+            onClick={() => setColor("lavender")} 
+          />
+          <Button 
+            colorName="White" 
+            bgColor="white" 
+            onClick={() => setColor("white")} 
+          />
+          <Button 
+            colorName="Black" 
+            bgColor="black" 
+            textColor='white'
+            onClick={() => setColor("black")} 
           />
         </div>
       </div>
--- bgchanger/src/components/button.jsx
+++ bgchanger/src/components/button.jsx
@@ -1,9 +1,9 @@
-export default function Button({ colorName = "Color", bgColor = "white", onClick }) {
+export default function Button({ colorName = "Color", textColor = "black", bgColor = "white", onClick }) {
   return (
     <button 
       onClick={onClick}
-      style={{ backgroundColor: bgColor }} 
-      className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+      style={{ backgroundColor: bgColor, color: textColor }} 
+      className="outline-none px-4 py-1 rounded-full shadow-lg cursor-pointer"
     >
       {colorName}
     </button>
