2fa706c22e2524763a2df539f4db4a316b47fc6b
AakifCodes <akifalvi2005@gmail.com>
2026-08-16 14:56:49 +0500

    Project Completed

--- 05passwordGenerator/src/components/button.jsx
+++ 05passwordGenerator/src/components/button.jsx
@@ -1,5 +1,5 @@
 export default function Button({onClick}){
     return(
-        <button onClick={onClick} className="outline-none bg-blue-700 text-white p-4 py-1 m-4 hover:bg-blue-800 rounded shrink-0 transition-colors">Copy</button>
+        <button onClick={onClick} className="outline-none bg-blue-600 text-white p-4 py-1 m-4 hover:bg-blue-800 rounded shrink-0 transition-colors">Copy</button>
     )
 }
