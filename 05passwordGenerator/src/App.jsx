079a6cf06d5a8f2ffbf3b7baf407ee9892edbac7
AakifCodes <akifalvi2005@gmail.com>
2026-08-16 10:31:53 +0500

    Create input field to select length of password

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -40,10 +40,21 @@ function App() {
           <Button />
 
         </div>
-        <div>
-          <div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="range" 
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e) => {setLength(e.target.value)}}
+
+            />
+            <label>Length : {length}</label>
 
           </div>
+          
         </div>
       </div>
     </>
