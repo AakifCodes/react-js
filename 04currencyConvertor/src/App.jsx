6154e12b138b121070777d294b42ae4560c60f78
AakifCodes <akifalvi2005@gmail.com>
2026-08-20 07:44:44 +0500

    Use custom hook

--- 04currencyConvertor/src/App.jsx
+++ 04currencyConvertor/src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 import { InputBox } from './components/index'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("pkr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  const currencyInfo = useCurrencyInfo(from)
 
   return (
     <>
