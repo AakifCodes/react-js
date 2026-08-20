07bad428dcda354812d2ac5a8ec052298a8f4dde
AakifCodes <akifalvi2005@gmail.com>
2026-08-20 08:42:52 +0500

    Project completed

--- 04currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 04currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
-    useEffect(() => {
-        fetch(`https://v6.exchangerate-api.com/v6/ebf9231c69f0534e9fa80479/latest/${currency}`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-    }, [currency])
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
 
-    return Data
+  useEffect(() => {
+    // API currency codes uppercase maangti hai (e.g. USD)
+    fetch(`https://v6.exchangerate-api.com/v6/ebf9231c69f0534e9fa80479/latest/${currency.toUpperCase()}`)
+      .then((res) => res.json())
+      .then((res) => setData(res.conversion_rates || {}))
+      .catch((err) => console.error("Error fetching currency rates:", err));
+  }, [currency]);
 
+  return data;
 }
 
-export default useCurrencyInfo
+export default useCurrencyInfo;
