be3c4eecc817ed83b0b940e35a7ace6f6b7a5080
AakifCodes <akifalvi2005@gmail.com>
2026-08-24 11:54:07 +0500

    Add new Github Component and call in header

---
+++ 06reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,5 @@
+export default function Github(){
+    return(
+        <div className="text-center p-4 bg-gray-600 text-white text-3xl">Github Followers</div>
+    )
+}
--- 06reactRouter/src/components/index.js
+++ 06reactRouter/src/components/index.js
@@ -4,5 +4,6 @@ import Home from "./Home/Home";
 import About from "./About/About";
 import Contact from "./Contact/Contact"
 import User from "./User/User";
+import Github from "./Github/Github";
 
-export { Footer, Header, Home, About, Contact, User }
+export { Footer, Header, Home, About, Contact, User, Github }
