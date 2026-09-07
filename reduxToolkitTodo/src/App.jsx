f7e80c21f9c1780b569755f94d3df7373c1495f3
AakifCodes <akifalvi2005@gmail.com>
2026-09-07 19:27:58 +0500

    Final Changes

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -3,13 +3,14 @@ import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
 
 function App() {
-  
   return (
-    <>
-      <h1>Learn about redux toolkit</h1>
+    <div className="min-h-screen bg-[#6B7280] flex flex-col items-center justify-start pt-10 px-4">
+      <h1 className="text-2xl font-semibold text-gray-900 text-center mb-2">
+        Learn about redux toolkit
+      </h1>
       <AddTodo />
       <Todos />
-    </>
+    </div>
   )
 }
 
--- reduxToolkitTodo/src/components/Todos.jsx
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import { removeTodo } from '../features/todo/todoSlice'
 
 function Todos() {
-    const todos = useSelector(state => state.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos)
+  const dispatch = useDispatch()
 
   return (
-    <>
-    <div>Todos</div>
-    <ul className="list-none">
+    <div className="w-full max-w-4xl flex flex-col items-center mt-6">
+      <div className="text-xl font-medium text-gray-900 mb-2">Todos</div>
+      <ul className="list-none w-full">
         {todos.map((todo) => (
           <li
-            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded w-full"
             key={todo.id}
           >
-            <div className='text-white'>{todo.text}</div>
+            <div className="text-white">{todo.text}</div>
             <button
-             onClick={() => dispatch(removeTodo(todo.id))}
-              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md flex-shrink-0"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +38,7 @@ function Todos() {
           </li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
 
